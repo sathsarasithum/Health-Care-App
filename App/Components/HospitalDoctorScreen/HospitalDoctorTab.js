392d6e56a6f8f27c69bc931daca865c67b88e658
sathsarasithum <sathsaragms.21@uom.lk>
2024-12-25 00:36:39 +0530

    Adding custom tab(Hospital and Doctor) in HospitalDoctorListScreen

---
+++ App/Components/HospitalDoctorScreen/HospitalDoctorTab.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import Colors from '../../../assets/Shared/Colors'
+
+const HospitalDoctorTab = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+  return (
+    <View style={{marginTop:10}}>
+      <View style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'space-around',marginRight:10}}>
+        <TouchableOpacity 
+        style={[
+            activeIndex==0
+            ?styles.activeTab
+            :styles.inActiveTab
+        ]}
+        onPress={()=>setActiveIndex(0)}>
+            <Text style={[
+                activeIndex===0
+                ?styles.activeText
+                :styles.inActiveText
+            ]}>Hospital</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={[
+            activeIndex==1
+            ?styles.activeTab
+            :styles.inActiveTab
+        ]}
+        onPress={()=>setActiveIndex(1)}>
+            <Text style={[
+                activeIndex===1
+                ?styles.activeText
+                :styles.inActiveText
+            ]}>Doctors</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default HospitalDoctorTab
+
+const styles = StyleSheet.create({
+    activeText:{
+        textAlign:'center',
+        fontFamily:'appfont',
+        fontSize:18,
+        color:Colors.PRIMARY
+    },
+    inActiveText:{
+        textAlign:'center',
+        fontFamily:'appfont',
+        fontSize:18,
+        color:Colors.GREY
+    },
+    activeTab:{
+        borderBottomWidth:2,
+        borderBottomColor:Colors.PRIMARY,
+        padding:3,
+        width:'20%'
+    },
+    inActiveTab:{
+        borderBottomWidth:1,
+        borderBottomColor:Colors.GREY,
+        padding:3,
+        width:'20%'
+    }
+})
--- App/Screens/HospitalDoctorListScreen.js
+++ App/Screens/HospitalDoctorListScreen.js
@@ -2,14 +2,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import PageHeader from '../Components/Shared/PageHeader';
+import HospitalDoctorTab from '../Components/HospitalDoctorScreen/HospitalDoctorTab';
 
 const HospitalDoctorListScreen = () => {
 
   const param = useRoute().params;
   return (
     <View style={{padding:30}}>
-      <Text>{param?.categoryName}</Text>
+       
       <PageHeader title={param?.categoryName}/>
+      <HospitalDoctorTab />
     </View>
   )
 }
